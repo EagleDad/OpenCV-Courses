ed8d858348d13b76d9c606587334d18b439d35b5
Dirk Adler <dirk.adler@emhartglass.com>
2024-03-01 08:38:26 +0100

    Moved images to a common folder

--- applications/basicImageOperations/main.cpp
+++ applications/basicImageOperations/main.cpp
@@ -13,5 +13,20 @@ IGNORE_WARNINGS_POP
 
 int main( [[maybe_unused]] int argc, [[maybe_unused]] char** argv )
 {
+    // Create an empty image matrix
+    cv::Mat emptyMat = cv::Mat( 100, 200, CV_8UC1, cv::Scalar::all( 0 ) );
+
+    showMat( emptyMat, "Empty Mat" );
+
+    // Set all white
+    emptyMat.setTo( cv::Scalar::all( 255 ) );
+
+    showMat( emptyMat, "White Mat" );
+
+    // Create Mat same size
+    cv::Mat emptyOrg =
+        cv::Mat( emptyMat.size( ), emptyMat.type( ), cv::Scalar::all( 128 ) );
+
+    showMat( emptyOrg, "Gray Mat", true );
     return 0;
 }
